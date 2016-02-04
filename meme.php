20179acc3ebba266b6e7d3110e14dd0c0d1aa847
jerrylu17 <2016jlu@tjhsst.edu>
2016-02-04 03:12:09 -0500

    database functionality
    button that shows top 5 sounds. saves this to database php added

--- meme.php
+++ meme.php
@@ -1,8 +1,17 @@
-
-	<?php
-		$filename = "memedata.txt";
-		$_data = $_REQUEST["memes"]; 
-		echo $_data;
-
-		file_put_contents($filename, $_data);
-	?>
+<?php
+	$filename = "memedata.txt";
+	$rqst = $_REQUEST["rqst"];
+	$data = $_REQUEST["memes"];
+	if($rqst == 'load'){
+		if (file_exists($filename)){
+			echo file_get_contents($filename);
+		}
+		else{
+			echo $data;
+		}
+	}
+	else if($rqst == 'save'){
+		file_put_contents($filename, $data);
+		echo $data;
+	}
+?>
